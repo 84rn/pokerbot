c3cee829d96c1374f42d32cfb9c8be4c58a823ef
Bartek Bartyzel <barbart1@gmail.com>
2015-03-12 22:45:25 +0100

    Fixed log mutex

--- pokerbot/basic_log.c
+++ pokerbot/basic_log.c
@@ -22,44 +22,50 @@ void log_cleanup()
 void log_msg(_TCHAR *l, ...)
 {
 	if (enable_log)
-	{
+	{	
+		WaitForSingleObject(mutex, INFINITE);
 		_tprintf("> ");
 		va_list vl;
 		va_start(vl, l);
 		log(l, vl);
 		va_end(vl);
+		ReleaseMutex(mutex);
 	}
 }
 
 void log_error(_TCHAR *l, ...)
-{	
+{
+		WaitForSingleObject(mutex, INFINITE);
 		_tprintf("Error: ");
 		va_list vl;
 		va_start(vl, l);
 		log(l, vl);
 		va_end(vl);	
+		ReleaseMutex(mutex);
 }
 
 void log_dbg(_TCHAR *l, ...)
 {
 	if (enable_log)
 	{
+		WaitForSingleObject(mutex, INFINITE);
 		_tprintf("DEBUG: ");
 		va_list vl;
 		va_start(vl, l);
 		log(l, vl);
 		va_end(vl);
+		ReleaseMutex(mutex);
 	}
 }
 
 void log(_TCHAR *l, ...)
 {
-	WaitForSingleObject(mutex, INFINITE);
+	
 
 	va_list vl;
 	va_start(vl, l);
 	_vtprintf(l, vl);
 	va_end(vl);
 	
-	ReleaseMutex(mutex);
+	
 }
